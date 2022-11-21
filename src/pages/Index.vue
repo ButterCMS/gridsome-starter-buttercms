f6bcb28550c640ec65981a9b37e8ce963975e8f2
Marek Okša <moksa@securityscorecard.io>
2022-11-21 16:45:20 +0100

    Code cleanup

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,23 +5,9 @@
 </template>
 
 <script>
-import { useApiError } from '@/utils/hooks'
 import HomeView from '@/views/HomeView'
-import { ref } from 'vue'
-
-const { error: apiError } = useApiError()
-const errorsWhileGenerating = ref(false)
 
 export default {
   components: { HomeView },
-  data() {
-    return {
-      error: apiError || errorsWhileGenerating,
-      apiKeyExists: !!process.env.GRIDSOME_APP_BUTTER_CMS_API_KEY,
-    }
-  },
-  mounted() {
-    errorsWhileGenerating.value = this.$static.errors.totalCount > 0
-  },
 }
 </script>
