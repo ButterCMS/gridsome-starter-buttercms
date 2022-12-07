7c775e39abb96390b948a764d975066c54922c85
ViolanteCodes <maria@buttercms.com>
2022-12-07 12:27:00 -0500

    fix api token language

--- src/views/NoApiTokenView.vue
+++ src/views/NoApiTokenView.vue
@@ -4,11 +4,8 @@
       <div class="row align-items-center">
         <div class="col-xl-6 col-lg-6 col-md-10">
           <div class="hero-content">
-            <h1>404 Page not found</h1>
-            <p>
-              Check that your API token is correct or that the requested URL is
-              valid.
-            </p>
+            <h1>Incorrect Token</h1>
+            <p>Check that your ButterCMS API token is correct.</p>
           </div>
         </div>
         <div class="col-xxl-6 col-xl-6 col-lg-6">
