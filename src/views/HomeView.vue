c2682e5a1e1f4812d96e7093a53fb593771c94d0
Prokop Simek <prokop@dxheroes.io>
2023-01-10 09:35:30 +0100

    fix: homepage loading by slug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-if="pageData">
-      <template v-for="(item, index) in pageData.fields.body">
+      <template v-for="(item, index) in pageData.body">
         <hero-section
           v-if="item?.type === 'hero'"
           :key="index"
@@ -27,6 +27,45 @@
     </div>
   </div>
 </template>
+
+<static-query>
+  query {
+    homePageData: allHomePageData {
+      edges {
+        node {
+          fields {
+            seo {
+              title
+              description
+            },
+            body {
+              type,
+              fields {
+                headline,
+                subheadline,
+                image,
+                button_label,
+                button_url,
+                scroll_anchor_id,
+                image_position,
+                features{
+                  headline,
+                  description,
+                  icon
+                },
+                testimonial {
+                  quote,
+                  name,
+                  title
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  </static-query>
 
 <script>
 import { inject, nextTick, ref } from 'vue'
@@ -59,12 +98,17 @@ export default {
 
     try {
       const butterCMS = getButterCMS()
+
       const pageSlug =
         this.$route?.params?.slug || 'landing-page-with-components'
 
-      pageData.value = (
-        await butterCMS?.page.retrieve('landing-page', pageSlug)
-      )?.data.data
+      if (pageSlug !== 'landing-page-with-components') {
+        pageData.value = (
+          await butterCMS?.page.retrieve('landing-page', pageSlug)
+        )?.data.data.fields
+      } else {
+        pageData.value = this.$static.homePageData.edges[0].node.fields
+      }
 
       await nextTick()
       handleMounted()
@@ -77,7 +121,7 @@ export default {
       return
     }
 
-    return this.$seo(pageData.value.fields.seo)
+    return this.$seo(pageData.value.seo)
   },
 }
 </script>
