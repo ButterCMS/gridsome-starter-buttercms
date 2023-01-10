4eda580446444a2f0607dc944d9b6521e16ffef6
Prokop Simek <prokop@dxheroes.io>
2023-01-10 08:22:24 +0100

    fix: loading landing pages by slug
    fixes #4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-if="pageData">
-      <template v-for="(item, index) in pageData">
+      <template v-for="(item, index) in pageData.fields.body">
         <hero-section
           v-if="item?.type === 'hero'"
           :key="index"
@@ -27,45 +27,6 @@
     </div>
   </div>
 </template>
-
-<static-query>
-query {
-	homePageData: allHomePageData {
-    edges {
-      node {
-        fields {
-          seo {
-            title
-            description
-          },
-          body {
-            type,
-            fields {
-              headline,
-              subheadline,
-              image,
-              button_label,
-              button_url,
-              scroll_anchor_id,
-              image_position,
-              features{
-                headline,
-                description,
-                icon
-              },
-              testimonial {
-                quote,
-                name,
-                title
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-</static-query>
 
 <script>
 import { inject, nextTick, ref } from 'vue'
@@ -98,11 +59,12 @@ export default {
 
     try {
       const butterCMS = getButterCMS()
-      const pageSlug = this.$route?.params?.slug
+      const pageSlug =
+        this.$route?.params?.slug || 'landing-page-with-components'
 
-      pageData.value =
-        this.$static.homePageData.edges[0].node.fields.body ??
-        (await butterCMS?.page.retrieve('landing-page', pageSlug)?.data.data)
+      pageData.value = (
+        await butterCMS?.page.retrieve('landing-page', pageSlug)
+      )?.data.data
 
       await nextTick()
       handleMounted()
@@ -111,8 +73,11 @@ export default {
     }
   },
   metaInfo() {
-    const seoData = this.$static.homePageData.edges[0].node.fields.seo
-    return this.$seo(seoData)
+    if (!pageData.value) {
+      return
+    }
+
+    return this.$seo(pageData.value.fields.seo)
   },
 }
 </script>
